b4479f4d0f67c2c58b6f84ed032d9215215bafdb
Azonkeu <mazonkeu@gmail.com>
2022-04-10 23:11:00 +0100

    Create the query schema

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
 
-const server = new ApolloServer();
+const server = new ApolloServer({typeDefs});
 
 const port = process.env.port || 9000
 
