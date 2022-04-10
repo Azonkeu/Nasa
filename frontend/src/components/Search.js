9b9a5ebfa7dc1f9de4b35edd92b78596a0a512ed
Azonkeu <mazonkeu@gmail.com>
2022-04-11 00:26:56 +0100

    Connect the app to the server

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,10 +1,14 @@
 import React, { useState} from 'react';
+import HomePage from './HomePage';
 
 const Search = () => {
   const [search, setSearch] = useState();
   return (
     <div className="search">
       <input type="text" value={search ?? ""} placeholder="Search..." onChange={(e) => setSearch(e.target.value)} className="inp" />
+      <div className="page">
+        <HomePage search={search} />
+      </div>
     </div>
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+} from "@apollo/client";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+export const client = new ApolloClient({
+  uri: 'http://localhost:9000/graphql',
+  cache: new InMemoryCache()
+});
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
 
 reportWebVitals();
